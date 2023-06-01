cdddc806aecb1a47fc163cb8d0358f4da8556fec
menna676 <menna.hanydice@gmail.com>
2023-06-01 07:13:15 -0700

    Team project

--- main.h
+++ main.h
@@ -50,6 +50,8 @@ int print_unsigned(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_octal(va_list types, char buffer[],
 int flags, int width, int precision, int size);
+int print_hexadecimal(va_list types, char buffer[],
+int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_hexa(va_list types, char map_to[],
